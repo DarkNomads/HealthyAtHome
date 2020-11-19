fdb485d20b7e067156ecbd5451833638fc5a3b03
David Cao <davidcao1337@gmail.com>
2020-11-19 16:21:03 -0500

    Organized code and features, see notes for details.

--- Python Component/main.py
+++ Python Component/main.py
@@ -1,54 +1,75 @@
-# Main Program
+# Calorie & Macro Tracker
 
-# Graphing Library
+# Libraries
+import datetime
+import calendar
 import matplotlib
 
+# Functions
 
-# TODO
+
 def display_menu():
-    print("*-----Calorie Tracker-----*")
-    print("1. Calorie intake entry")
-    print("2. Calorie burned entry")
-    print("3. Calculate net calorie gain/loss")
+    print("\n*-----Calorie Tracker-----*")
+    print("1. Set calorie goal (daily)")
+    print("2. Calculate today's net calorie")
+    print("3. Display this week's progress")
     print("4. Quit program")
 
 
-def net_calorie(cal_in, cal_burn):
+def calc_net_cal(weekly_cal, curr_day, cal_goal):
+    # User enters intake/burn
+    cal_in = int(input("Enter calorie intake: "))
+    cal_burn = int(input("Enter calorie burned: "))
+    # Calculate net calories
     net_cal = cal_in - cal_burn
-    return net_cal
+    # Store net calorie information
+    weekly_cal[curr_day] = net_cal
+    # Display today's net calorie & compare with goal
+    print("\nToday's net calorie:", net_cal)
+    print("Daily calorie goal:", cal_goal)
+    # Calc difference between actual net and goal
+    cal_diff = net_cal - cal_goal
+    if cal_diff == 0:
+        print("Daily calories met.")
+    elif cal_diff > 0:
+        print("Daily calories exceeded goal.")
+    else:
+        print("Daily calories not met.")
 
 
 # TODO
 def graph():
     pass
 
+# --------------------------------------------------------------
 
-# ------------------------------------------------
+# Main Program
 
-# Demo
 
-# Total calorie data
-total_cal_in = 0
-total_cal_burn = 0
+# Get day of the week; 0 = Monday ... 6 = Sunday
+curr_day = datetime.date.today().weekday()
+
+# Weekly calories
+weekly_cal = [0, 0, 0, 0, 0, 0, 0]
+week = list(calendar.day_name)
+
+# User calorie goal (daily)
+cal_goal = 0
 
 while True:
     display_menu()
     menu_choice = input("\nSelection: ")
 
     if menu_choice == "1":
-        total_cal_in = int(input("Enter total calorie intake: "))
+        cal_goal = int(input("Enter your daily calorie goal: "))
     elif menu_choice == "2":
-        total_cal_burn = int(input("Enter total calorie burned: "))
+        calc_net_cal(weekly_cal, curr_day, cal_goal)
     elif menu_choice == "3":
-        net_cal = net_calorie(total_cal_in, total_cal_burn)
-        if net_cal < 0:
-            print("Net calorie loss:", abs(net_cal))
-        else:
-            print("Net calorie gained:", net_cal)
+        print("\n*-----Weekly Progress-----*")
+        for day in range(7):
+            print(week[day] + ": " + str(weekly_cal[day]))
     elif menu_choice == "4":
         exit(0)
     else:
-        print("Invalid input...please try again\n")
-
-
+        print("Invalid input...please try again")
 
